a08c8c45522087aa8d18761f569f717ba182d1f7
hookeyy <143789490+hookeyy@users.noreply.github.com>
2023-11-20 13:26:24 +0100

    Add files via upload

---
+++ funny.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+count=0
+
+while [ $count -lt 1000 ]
+do
+  open "http://www.pornhub.com"
+  open "http://www.xnxx.com"
+  open "http://www.xvideos.com" 
+  open "http://www.porn.com"
+  open "http://www.sex.com"
+  ((count++))
+done
